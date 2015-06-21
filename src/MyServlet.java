f389fda8102fc64d67186701e344e0b9f7e92aa2
Rajeev09 <reddirajeev@gmail.com>
2015-06-21 22:28:20 +0530

    first servlet
    hello

---
+++ src/MyServlet.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;  
+import javax.servlet.*;  
+import java.io.*; 
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		ServletOutputStream out = response.getOutputStream();
+
+		// set content type and other response header fields first
+		response.setContentType("text/html");
+
+		// then write the data of the response
+	    out.println("<HEAD><TITLE> SimpleServlet Output </TITLE></HEAD><BODY>");
+		out.println("<h1> SimpleServlet Output </h1>");
+		out.println("<P>This is output from SimpleServlet.");
+		out.println("</BODY>");
+		out.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
